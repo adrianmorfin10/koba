d0d56e57a2b86e67bde688cba6506a4311f0e8ad
adrianmorfin10 <adri_mo95@hotmail.com>
2025-06-26 20:25:13 -0600

    ssjsjso

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  reactStrictMode: true,
+  images: {
+    unoptimized: true,
+    domains: ['http2.mlstatic.com'],
+  }
+}
 
-export default nextConfig;
+export default nextConfig
